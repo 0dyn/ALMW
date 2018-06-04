b80e97f987e57691532587eb43e479e48864fd1f
0dynTheMutant <soczek1890@gmail.com>
2018-06-05 00:08:58 +0200

    Kontrolery -admin error register home

---
+++ src/main/java/pl/AdminController.java
@@ -0,0 +1,21 @@
+package pl.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import pl.model.User;
+
+@Controller
+public class AdminController {
+
+	@GetMapping("/admin")
+	public String register(Model model)
+    {
+		CheckAuth user = new CheckAuth(model);
+		return user.checkPracownik("admin");
+    }
+}
+ 
